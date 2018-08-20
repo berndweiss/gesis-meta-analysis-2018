ae5728a60fd1d256176c7b41889766b06c806fd2
Bernd Weiss <spam@metaanalyse.de>
2018-08-20 04:28:27 +0200

    add arnim's postBuild

--- binder/install.R
+++ binder/install.R
@@ -1,6 +1,5 @@
 install.packages("car", repos='http://cran.us.r-project.org')
 install.packages("metafor", repos='http://cran.us.r-project.org')
-install.packages("repr", repos='http://cran.us.r-project.org')
 ## install.packages("readr", repos='http://cran.us.r-project.org')
 ## install.packages("sodium", repos='http://cran.us.r-project.org')
 ## install.packages("yaml", repos='http://cran.us.r-project.org')
