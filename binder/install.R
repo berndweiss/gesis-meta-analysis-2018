7b22e82a1519a54c5f257b5da551e272f5a86277
Bernd Weiss <spam@metaanalyse.de>
2018-08-23 13:51:15 +0200

    update exercise 4-1

--- binder/install.R
+++ binder/install.R
@@ -1,4 +1,5 @@
-install.packages("car", repos='http://cran.us.r-project.org')
+## install.packages("car", repos='http://cran.us.r-project.org')
+install.packages("car")
 install.packages("metafor", repos='http://cran.us.r-project.org')
 ## install.packages("readr", repos='http://cran.us.r-project.org')
 ## install.packages("sodium", repos='http://cran.us.r-project.org')
